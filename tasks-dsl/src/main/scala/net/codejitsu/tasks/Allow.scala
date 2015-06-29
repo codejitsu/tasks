dbe5d36cf5d64f7446d127fd9bf4572f7475d6f8
Alex <me@codejitsu.net>
2015-06-29 06:07:08 +0200

    Added AllowTest.

--- tasks-dsl/src/main/scala/net/codejitsu/tasks/Allow.scala
+++ tasks-dsl/src/main/scala/net/codejitsu/tasks/Allow.scala
@@ -6,17 +6,47 @@ import net.codejitsu.tasks.dsl.TaskM
 
 import scala.annotation.implicitNotFound
 
-trait Stage
-trait Dev extends Stage
-trait Production extends Stage
+trait Stage {
+  def name: String
+  override def toString: String = name
+}
+
+trait Dev extends Stage {
+  override val name: String = "Development"
+}
+
+trait Test extends Stage {
+  override val name: String = "Test"
+}
+
+trait QA extends Stage {
+  override val name: String = "QA"
+}
+
+trait Production extends Stage {
+  override val name: String = "Production"
+}
 
 @implicitNotFound("Stage '${S}' is not allowed to run task '${T}'.")
 class Allow[S <: Stage, T <: TaskM[Boolean]]
 
 object Allow {
-  implicit val devAllowAll: Dev Allow TaskM[Boolean] = new Allow[Dev, TaskM[Boolean]]
-
-  // scalastyle:off
-  implicit val prodAllowNothing: Production Allow TaskM[Boolean] = null
-  // scalastyle:on
+  //all tasks enabled per default in Dev
+  implicit val devAllowTouch: Dev Allow Touch[Dev] = new Allow[Dev, Touch[Dev]]
+  implicit val devAllowWait: Dev Allow Wait[Dev] = new Allow[Dev, Wait[Dev]]
+  implicit val devAllowUpload: Dev Allow Upload[Dev] = new Allow[Dev, Upload[Dev]]
+  implicit val devAllowCheckUrl: Dev Allow CheckUrl[Dev] = new Allow[Dev, CheckUrl[Dev]]
+  implicit val devAllowCp: Dev Allow Cp[Dev] = new Allow[Dev, Cp[Dev]]
+  implicit val devAllowDownload: Dev Allow Download[Dev] = new Allow[Dev, Download[Dev]]
+  implicit val devAllowMkdir: Dev Allow Mkdir[Dev] = new Allow[Dev, Mkdir[Dev]]
+  implicit val devAllowMv: Dev Allow Mv[Dev] = new Allow[Dev, Mv[Dev]]
+  implicit val devAllowRm: Dev Allow Rm[Dev] = new Allow[Dev, Rm[Dev]]
+  implicit val devAllowRmIfExists: Dev Allow RmIfExists[Dev] = new Allow[Dev, RmIfExists[Dev]]
+  implicit val devAllowGetRequest: Dev Allow GetRequest[Dev] = new Allow[Dev, GetRequest[Dev]]
+  implicit val devAllowPostRequest: Dev Allow PostRequest[Dev] = new Allow[Dev, PostRequest[Dev]]
+  implicit val devAllowInstallDeb: Dev Allow InstallDeb[Dev] = new Allow[Dev, InstallDeb[Dev]]
+  implicit val devAllowStartService: Dev Allow StartService[Dev] = new Allow[Dev, StartService[Dev]]
+  implicit val devAllowStopService: Dev Allow StopService[Dev] = new Allow[Dev, StopService[Dev]]
+  implicit val devAllowStartTomcat: Dev Allow StartTomcat[Dev] = new Allow[Dev, StartTomcat[Dev]]
+  implicit val devAllowStopTomcat: Dev Allow StopTomcat[Dev] = new Allow[Dev, StopTomcat[Dev]]
 }
---
+++ tasks-dsl/src/test/scala/AllowTest.scala
@@ -0,0 +1,48 @@
+package net.codejitsu.tasks.dsl
+
+import net.codejitsu.tasks._
+import net.codejitsu.tasks.dsl.Tasks._
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Allow tests.
+ */
+class AllowTest extends FlatSpec with Matchers {
+
+  import scala.concurrent.duration._
+
+  implicit val timeout = 30 seconds
+
+  "For a stage" should "not allow tasks without allow rule defined" in {
+    case class TestTask1[S <: Stage](implicit user: User, stage: S, rights: S Allow TestTask1[S])
+      extends GenericTask("test", "test", Localhost, "test", List(),
+        false, false)
+
+    implicit val stage = new Dev {}
+
+    assertTypeError("val res = TestTask1().run()")
+  }
+
+  "For a stage" should "allow tasks with allow rule defined" in {
+    case class TestTask2[S <: Stage](implicit user: User, stage: S, rights: S Allow TestTask2[S])
+      extends GenericTask("test", "test", Localhost, "test", List(),
+        false, false)
+
+    implicit val stage = new Dev {}
+    implicit val allowTestTask2: Dev Allow TestTask2[Dev] = new Allow[Dev, TestTask2[Dev]]
+
+    TestTask2().run()
+
+    //it should compile
+  }
+
+  "For a stage" should "select the stage rule correctly" in {
+    case class TestTask3[S <: Stage](implicit user: User, stage: S, rights: S Allow TestTask3[S])
+      extends GenericTask("test", "test", Localhost, "test", List(),
+        false, false)
+
+    implicit val stage = new Production {}
+    implicit val allowDevTestTask3: Dev Allow TestTask3[Dev] = null //null is the correct value
+    assertTypeError("val res = TestTask3().run()")
+  }
+}
