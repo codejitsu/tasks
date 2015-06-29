8fdc7292699244e241ebcfe314362f2a562dc6e2
Alex <me@codejitsu.net>
2015-06-29 06:23:21 +0200

    Refactoring.

--- tasks-dsl/src/main/scala/net/codejitsu/tasks/dsl/Allow.scala
+++ tasks-dsl/src/main/scala/net/codejitsu/tasks/dsl/Allow.scala
@@ -1,8 +1,8 @@
 // Copyright (C) 2015, codejitsu.
 
-package net.codejitsu.tasks
+package net.codejitsu.tasks.dsl
 
-import net.codejitsu.tasks.dsl.{Dev, Stage, TaskM}
+import net.codejitsu.tasks._
 
 import scala.annotation.implicitNotFound
 
